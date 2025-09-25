a65a83f1e90eea87d830ea848dec358b8b1ccc49
Fadell-Karlsefni <fadilah3109@gmail.com>
2025-09-25 20:34:22 +0800

    Perbaiki error di uuid_array.go

--- models/types/uuid_array.go
+++ models/types/uuid_array.go
@@ -12,6 +12,8 @@ import (
 type UUIDArray []uuid.UUID
 
 func (a *UUIDArray) Scan(value interface{}) error {
+
+	//{213123asdad,asdasda,123123}
 	var str string
 
 	switch v := value.(type) {
@@ -27,34 +29,38 @@ func (a *UUIDArray) Scan(value interface{}) error {
 	str = strings.TrimSuffix(str, "}")
 	parts := strings.Split(str, ",")
 
-	// make([]T,length,capacity)
+	//make([]T,length , capacity)
 	*a = make(UUIDArray, 0, len(parts))
 	for _, s := range parts {
-		s = strings.TrimSpace(strings.Trim(s, `"`)) // akan menghapus spasi dan kutip
+		s = strings.TrimSpace(strings.Trim(s, `"`)) // akan menghapus spasi dan "
 		if s == "" {
 			continue
 		}
 		u, err := uuid.Parse(s)
 		if err != nil {
-			return fmt.Errorf("invalid UUID in array : %v", err)
+			return fmt.Errorf("invalid UUID in Array : %v", err)
 		}
 		*a = append(*a, u)
+
 	}
 	return nil
+
 }
 
-func (a UUIDArray) value() (driver.Value, error) {
+//{"550e8400-e29b-41d4-a716-446655440000","123e4567-e89b-12d3-a456-426614174000"}
+
+func (a UUIDArray) Value() (driver.Value, error) {
 	if len(a) == 0 {
 		return "{}", nil
 	}
-
 	postgreFormat := make([]string, 0, len(a))
 	for _, value := range a {
 		postgreFormat = append(postgreFormat, fmt.Sprintf(`"%s"`, value.String()))
 	}
 	return "{" + strings.Join(postgreFormat, ",") + "}", nil
+
 }
 
-func (UUIDArray) gormDataType() string {
+func (UUIDArray) GormDataType() string {
 	return "uuid[]"
 }
