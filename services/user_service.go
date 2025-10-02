605349d6cbc0f217525eaa7790a4b7d83482f643
Fadell-Karlsefni <fadilah3109@gmail.com>
2025-10-02 15:01:55 +0800

    Menambahkan comment di user_service

--- services/user_service.go
+++ services/user_service.go
@@ -22,16 +22,24 @@ func NewUserService(repo repositories.UserRepositroy) UserService {
 }
 
 func (s *userService) Register(user *models.User) error {
+
+	// Cek alamat email sudah terdaftar atau belum
 	existingUser, _ := s.repo.FindByEmail(user.Email)
 	if existingUser.InternalID != 0 {
 		return errors.New("email already registered")
 	}
+
+	// hash	password
 	hased, err := utils.HashPassword(user.Password)
 	if err != nil {
 		return err
 	}
 	user.Password = hased
+
+	// set default role & publicID
 	user.Role = "user"
 	user.PublicID = uuid.New()
+
+	// simpan ke DB lewat Repository
 	return s.repo.Create(user)
 }
