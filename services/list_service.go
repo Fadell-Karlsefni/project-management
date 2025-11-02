dd1c0c88c96ed3834b5525e9da27d127abec69b3
Fadell-Karlsefni <fadilah3109@gmail.com>
2025-11-03 00:07:18 +0800

    menambahkan struktur dan interface untuk layanan list pada board: - Menambahkan struct ListWithOrder untuk menyimpan data list beserta urutan posisinya - Menambahkan interface ListService dengan method GetByBoardID, GetByID, GetByPublicID, Create, Update, Delete, dan UpdatePositions

--- services/list_service.go
+++ services/list_service.go
@@ -1,9 +1,28 @@
 package services
 
-import "github.com/Fadell-Karlsefni/project-management/repositories"
+import (
+	"github.com/Fadell-Karlsefni/project-management/models"
+	"github.com/Fadell-Karlsefni/project-management/repositories"
+	"github.com/google/uuid"
+)
 
-type ListService struct {
+type listService struct {
 	listRepo repositories.ListRepository
 	boardRepo repositories.BoardRepository
-	
+	listPosRepo repositories.ListPositionRepository
+}
+
+type ListWithOrder struct {
+	Position []uuid.UUID
+	Lists []models.List
+}
+
+type ListService interface {
+	GetByBoardID(boardPublicID string) (*ListWithOrder,error)
+	GetByID(id uint) (*models.List, error)
+	GetByPublicID(publicID string) (*models.List,error)
+	Create(list *models.List) error
+	Update(list *models.List) error
+	Delete(id uint) error
+	UpdatePositions(boardPublicID string, position []uuid.UUID) error 
 }
