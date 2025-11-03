2b2b8a126b67d20df03cde64808cd216434facbe
Fadell-Karlsefni <fadilah3109@gmail.com>
2025-11-03 10:38:07 +0800

    fitur(list): tambahkan service GetByBoardID dan utilitas sorting list - Menambahkan method FindByBoardID dan FindByPublicID di ListRepository - Mengimplementasikan GetByBoardID di ListService (termasuk validasi board dan urutan posisi) - Membuat fungsi utils.SortListByPosition untuk mengurutkan list berdasarkan posisi - Memperbarui konstruktor NewServiceList agar menginisialisasi repository dengan benar

--- services/list_service.go
+++ services/list_service.go
@@ -1,8 +1,11 @@
 package services
 
 import (
+	"errors"
+
 	"github.com/Fadell-Karlsefni/project-management/models"
 	"github.com/Fadell-Karlsefni/project-management/repositories"
+	"github.com/Fadell-Karlsefni/project-management/utils"
 	"github.com/google/uuid"
 )
 
@@ -25,4 +28,58 @@ type ListService interface {
 	Update(list *models.List) error
 	Delete(id uint) error
 	UpdatePositions(boardPublicID string, position []uuid.UUID) error 
+}
+
+func NewServiceList(listRepo repositories.ListRepository, boardRepo repositories.BoardRepository, listPosRepo repositories.ListPositionRepository) ListService {
+	return &listService{listRepo,boardRepo,listPosRepo,}
+}
+
+func (s *listService) GetByBoardID(boardPublicID string) (*ListWithOrder, error) {
+	// verifikasi board
+
+	_, err :=  s.boardRepo.FindByPublicID(boardPublicID)
+	if err != nil {
+		return nil, errors.New("board not found")
+	}
+
+	positions, err := s.listPosRepo.GetListOrder(boardPublicID)
+	if err != nil {
+		return nil, errors.New("failed to get list order : " + err.Error())
+	}
+
+	lists, err := s.listRepo.FindByBoardID(boardPublicID)
+	if err != nil {
+		return nil, errors.New("failed to get list : " + err.Error())
+	}
+
+	//sorting by position
+	orderedList :=  utils.SortListByPosition(lists,positions)
+	return &ListWithOrder{
+		Position: positions,
+		Lists: orderedList,
+	},nil
+}
+
+func (s *listService) GetByID(id uint) (*models.List, error) {
+	return nil, nil
+}
+
+func (s *listService) GetByPublicID(publicID string) (*models.List, error) {
+	return nil, nil
+}
+
+func (s *listService) Create(list *models.List) error {
+	return nil
+}
+
+func (s *listService) Update(list *models.List) error {
+	return nil
+}
+
+func (s *listService) Delete(id uint) error {
+	return nil
+}
+
+func (s *listService) UpdatePositions(boardPublicID string, position []uuid.UUID) error {
+	return nil
 }
